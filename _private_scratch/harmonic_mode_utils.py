b77364b876894cf513bf0fd8fff88ab1a107ac3d
Seth Olsen <setholsenr@gmail.com>
2021-11-27 13:19:29 -0500

    moved function for analytic form of fplus fcross into a new private module called non_lvc_conventions.py

--- _private_scratch/harmonic_mode_utils.py
+++ _private_scratch/harmonic_mode_utils.py
@@ -99,3 +99,18 @@ def A_lm_inclin(l, m, iota):
 def Y_lm(l, m, iota, azim):
     """NOTE: azim = pi/2 - vphi"""
     return np.exp(1j * m * azim) * A_lm_inclin(l, m, iota)
+
+
+def fplus_fcross_analytic(det_polar, det_azimuth, psi):
+    """
+    Get time-indep. geometric F+, Fx following https://arxiv.org/pdf/1102.5421.pdf
+    where det_polar, det_azimuth are the polar and azimuthal angles of the
+    line-of-sight in the frame whose x-y plane is defined by the detector arms
+    with the z-axis pointing toward the sky.
+    """
+    costheta = np.cos(det_polar)
+    part1 = 0.5 * (1 + (costheta ** 2)) * np.cos(2 * det_azimuth)
+    part2 = costheta * np.sin(2 * det_azimuth)
+    c2ps = np.cos(2 * psi)
+    s2ps = np.sin(2 * psi)
+    return part1 * c2ps - part2 * s2ps, part1 * s2ps + part2 * c2ps
