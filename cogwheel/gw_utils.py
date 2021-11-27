b31af2f2b3ef2169b17dc144a6b5d4795221f9e2
Seth Olsen <setholsenr@gmail.com>
2021-11-26 19:22:28 -0500

    fixed fplus_fcross_analytic to match LVC convention

--- cogwheel/gw_utils.py
+++ cogwheel/gw_utils.py
@@ -36,6 +36,7 @@ def fplus_fcross_detector(detector_name, ra, dec, psi, tgps):
 def fplus_fcross_analytic(det_polar, det_azimuth, psi):
     """
     Get time-indep. geometric F+, Fx following https://arxiv.org/pdf/1102.5421.pdf
+    (but taking psi -> -psi to match LVC convention)
     where det_polar, det_azimuth are the polar and azimuthal angles of the
     line-of-sight in the frame whose x-y plane is defined by the detector arms
     with the z-axis pointing toward the sky
@@ -44,7 +45,7 @@ def fplus_fcross_analytic(det_polar, det_azimuth, psi):
     part1 = 0.5 * (1 + (costheta ** 2)) * np.cos(2 * det_azimuth)
     part2 = costheta * np.sin(2 * det_azimuth)
     c2ps = np.cos(2 * psi)
-    s2ps = np.sin(2 * psi)
+    s2ps = -np.sin(2 * psi)
     return part1 * c2ps - part2 * s2ps, part1 * s2ps + part2 * c2ps
 
 def time_delay_from_geocenter(detector_names, ra, dec, tgps):
