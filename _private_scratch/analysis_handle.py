377884a049516a0d92796dd68f63095ffc80f2a4
Seth Olsen <setholsenr@gmail.com>
2021-11-30 02:11:51 -0500

    fixed bug in corner_plot_comparison argument compare_names

--- _private_scratch/analysis_handle.py
+++ _private_scratch/analysis_handle.py
@@ -438,16 +438,17 @@ class AnalysisHandle:
         multigrid_kwargs will be unpacked into MultiGrid.from_grids().
         extra_grid_kwargs will be unpacked into Grid.from_samples().
         """
-        if len(compare_names) < len(compare_posteriors):
-            compare_names += [''] * (len(compare_posteriors) - len(compare_names))
+        comp_names = dcopy(compare_names)
+        if len(comp_names) < len(compare_posteriors):
+            comp_names += [''] * (len(compare_posteriors) - len(comp_names))
         for j in range(len(compare_posteriors)):
             if isinstance(compare_posteriors[j], AnalysisHandle):
-                if compare_names[j] == '':
-                    compare_names[j] = compare_posteriors[j].name
+                if comp_names[j] == '':
+                    comp_names[j] = compare_posteriors[j].name
                 compare_posteriors[j] = compare_posteriors[j].samples
         return peplot.corner_plot_list(([self.masked_samples(key_rngs)] +
                                         [s[key_rngs_mask(s, key_rngs)] for s in compare_posteriors]),
-                                       [self.name] + compare_names, pvkeys=parkeys, weight_key=weight_key,
+                                       [self.name] + comp_names, pvkeys=parkeys, weight_key=weight_key,
                                        grid_kws=extra_grid_kwargs, multigrid_kws=multigrid_kwargs,
                                        return_grid=return_grid, **corner_plot_kwargs)
 
