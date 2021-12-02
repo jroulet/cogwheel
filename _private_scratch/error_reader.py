08bb84469047a4262abf910a4183ac1793623be7
Seth Olsen <setholsenr@gmail.com>
2021-12-02 10:05:48 -0500

    updated ErrorReader.print() to account for when len(readlines)=print_tail and added options line_contains='' and path_contains='' to filter what is printed

--- _private_scratch/error_reader.py
+++ _private_scratch/error_reader.py
@@ -1,9 +1,15 @@
+"""
+Module for reading .err and .out files in directory and its subdirect"""
 import os
+
+def line_empty(line):
+    return (len(line.strip()) > 0)
 
 def is_xphm_warning(x):
     return (('XLAL Error - IMRPhenomX' in x) and
             (('Defaulting to NNLO angles' in x)
              or ('Triggering MSA failure' in x)))
+
 
 class ErrorReader:
     """Class for reading error file and other textual output"""
@@ -49,28 +55,36 @@ class ErrorReader:
     def get_outfile_paths(self, dir=None, subdirs=True):
         return self.get_paths(dir=dir, subdirs=subdirs, ext='.out')
 
-    def get_lines(self, paths=None, keep_empty=False,
-                  keep_xphm_warnings=False):
-        lineok0 = ((lambda x: True) if keep_empty else
-                   (lambda x: (len(x.strip()) > 0)))
+    def get_lines(self, paths=None, path_contains='', line_contains='',
+                  keep_empty=False, keep_xphm_warnings=False):
+        lineok0 = ((lambda x: (line_contains in x)) if keep_empty else
+                   (lambda x: ((line_contains in x) and line_empty(x))))
         lineok = ((lambda x: lineok0(x)) if keep_xphm_warnings else
                   (lambda x: (lineok0(x) and (not is_xphm_warning(x)))))
         lines = {}
-        for p in [str(p) for p in (paths or self.errorfile_paths)]:
+        for p in [str(p) for p in (paths or self.errorfile_paths)
+                  if path_contains in p]:
             lines[p] = [l for l in open(p, 'r').readlines() if lineok(l)]
         return lines
 
-    def print(self, print_tail, paths=None, get_lines=True):
+    def print(self, print_tail, paths=None, get_lines=True,
+              path_contains='', line_contains='', keep_empty=False,
+              keep_xphm_warnings=False):
         pathstrs = [str(p) for p in (paths or self.errorfile_paths)]
         ntail = int(print_tail)
         print(f'....Printing {ntail} lines for {len(pathstrs)} files....')
         if get_lines:
-            self.lines.update(self.get_lines(pathstrs))
+            self.lines.update(self.get_lines(pathstrs, keep_empty=keep_empty,
+                path_contains=path_contains, line_contains=line_contains,
+                keep_xphm_warnings=keep_xphm_warnings))
         for i, p in enumerate(pathstrs):
-            nlines = len(self.lines[p])
-            print(f'[{i}]\t' + '-' * 64 + f'\n{p}:\n({nlines} lines)')
-            for jback in range(min(ntail, nlines)):
-                print(f'[[-{ntail - jback}]] {self.lines[p][-(ntail - jback)]}')
+            if path_contains in p:
+                nlines = len(self.lines[p])
+                print(f'[{i}]\t' + '-' * 64 + f'\n{p}:\n({nlines} lines)')
+                for jback in range(min(ntail, nlines)):
+                    backind = min(ntail, nlines) - jback
+                    if line_contains in self.lines[p][-backind]:
+                        print(f'[[-{backind}]] {self.lines[p][-backind]}')
 
     def archive_paths(self, paths=None):
         for p in [str(p) for p in (paths or self.errorfile_paths)]:
