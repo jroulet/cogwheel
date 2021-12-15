aebb5da377ad1802a1d7af0a796f266bd6ee147c
Seth Olsen <setholsenr@gmail.com>
2021-12-14 21:38:07 -0500

    fixed typo in analysis_handle

--- _private_scratch/analysis_handle.py
+++ _private_scratch/analysis_handle.py
@@ -406,7 +406,8 @@ class AnalysisHandle:
         ylab = 'Waveform Amplitude'
         if cumsum:
             ylab = r"$\int^{f} | h_w(f') |^2 \rm{d}f'$"
-            h_f = np.cumsum(self.likelihood._compute_h_h(h_f), axis=-1)
+            h_f = np.cumsum((4 * self.event_data.df * self.asd_drift**-2)[..., np.newaxis]
+                            * np.abs(h_f * self.event_data.wht_filter)**2, axis=-1)
         elif whiten:
             ylab = 'Whitened Waveform Amplitude'
             h_f = self.evdata.dt * np.fft.rfft(self.likelihood._get_whitened_td(h_f), axis=-1)
