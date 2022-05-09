fc56ef53f33a005941439318477bd85f607ea8aa
Javier Roulet <jroulet@princeton.edu>
2022-05-09 10:51:15 -0700

    Hotfix: added highpass filter to wht_filter in _private_ias.

--- _private_scratch/_private_ias.py
+++ _private_scratch/_private_ias.py
@@ -393,7 +393,8 @@ class EventMetadata(data.utils.JSONMixin):
         dic['tgps'] = use_tgps
         dic |= get_f_strain_psd_dic(triggerlists, use_tgps,
                                     self.tcoarse, self.t_interval)
-        dic['wht_filter'] = 1 / dic.pop('psd')
+        dic['wht_filter'] = (data.highpass_filter(dic['frequencies'])
+                             / dic.pop('psd'))
 
         return data.EventData(**dic)
 
