f786efc6359b2b941c93fda34e369e6dc9b78745
Javier Roulet <jroulet@princeton.edu>
2022-06-17 20:23:24 -0700

    Refactored jump_test into classes

--- cogwheel/jump_test.py
+++ cogwheel/jump_test.py
@@ -1,176 +1,193 @@
-import numpy as np
-import sys
+"""Tests that samples actually follow some probability distribution."""
 
-sys.path.append('..')
+from abc import ABC, abstractmethod
+import scipy.special
+import numpy as np
+import pandas as pd
+
 from cogwheel import gw_plotting
 
 
-def perform_jump_test(posterior, samples, jump_function,
-                      label='Original', weights_col='weights'):
-    nsamples = len(samples)
-    lnposterior = np.vectorize(posterior.lnposterior)
-    params = posterior.prior.sampled_params
-    jumped_samples = jump_function(samples)
-
-    relevant = ~np.all(samples == jumped_samples, axis=1)
-    samples = samples[relevant]
-    jumped_samples = jumped_samples[relevant]
-
-    lnprob = lnposterior(**samples[params])
-    lnprob_jumped = lnposterior(**jumped_samples[params])
-    # jump_probability = 1 / (1 + np.exp(lnprob - lnprob_jumped))
-    jump_probability = np.exp(lnprob_jumped - lnprob)
-
-    jump = np.random.uniform(0, 1, len(samples)) < jump_probability
-    print(f'{100 * np.count_nonzero(jump) / nsamples:.1f}% of samples jumped.')
-
-    test_samples = samples.copy()
-    test_samples[jump] = jumped_samples[jump]
-
-    gw_plotting.MultiCornerPlot.from_samples(
-        [samples, test_samples],
-        labels=[label, 'Jump test'],
-        weights_col=weights_col,
-        params=params
-        ).plot(tightness=.999, max_n_ticks=4)
-    return test_samples
-
-lnq0 = -2.  # Monkey-patchable
-@np.vectorize
-def _lnq_jump_aux(lnq):
-    """This function is its own inverse and its Jacobian is 1."""
-    lnq1 = lnq0 / 2
-    if lnq < lnq0:
-        return lnq
-    if lnq < lnq1:
-        return lnq + lnq1 - lnq0
-    return lnq - (lnq1 - lnq0)
-
-
-def lnq_jump_function(samples):
+class BaseJumpTest(ABC):
     """
-    Return a copy of a samples dataframe where the jump has been applied
-    to column 'lnq'.
+    Abstract class for performing tests for detailed balance.
+
+    The purpose of this class is to test whether a set of samples
+    comes from a distribution that we know how to evaluate. The test
+    consists in applying a transformation to the samples that produces a
+    second set of samples. Any histograms of either set of samples
+    should remain unchanged if the samples indeed come from the
+    distribution.
+    The test requires the user to provide a ``jump_function``, this can
+    be any bijective function on the space of folded sampled parameters
+    with the following properties:
+        * Unit Jacobian determinant
+        * Self inverse, i.e. f(f(x)) = x
+    Each sample ``x`` is randomly reassigned between ``x`` and ``f(x)``,
+    depending on the relative probabilities ``P(x)`` and ``P(f(x))``.
+    A good jump function would connect regions of the parameter space
+    with similar probabilities, and if you suspect that some particular
+    parameter may have been mis-sampled you should try to involve it in
+    the jump function. It is usually hard to devise a good jump function
+    that involves correlated parameters.
+    If a set of samples passes the jump test it does not mean it is
+    correct. If it does not pass the jump test it is incorrect.
     """
-    jumped_samples = samples.copy()
-    jumped_samples['lnq'] = _lnq_jump_aux(samples['lnq'])
-    return jumped_samples
+    def __init__(self, posterior, samples):
+        """
+        Parameters
+        ----------
+        posterior: cogwheel.posterior.Posterior
+
+        samples: pandas.DataFrame
+            Columns must contain ``posterior.prior.sampled_params``,
+            rows correspond to samples that allegedly come from
+            ``posterior.lnposterior``.
+        """
+        self.posterior = posterior
+        self.folded_samples = pd.DataFrame(
+            posterior.prior.fold(**samples[posterior.prior.sampled_params]),
+            columns=posterior.prior.sampled_params)
+
+        lnposterior_unfolds = posterior.prior.unfold_apply(
+            posterior.lnposterior)
+
+        @np.vectorize
+        def ln_folded_posterior(*folded_par_vals, **folded_par_dic):
+            """
+            Return the natural logarithm of the folded posterior
+            density. Inputs must live in the space of folded sampled
+            parameters.
+            """
+            return scipy.special.logsumexp(
+                lnposterior_unfolds(*folded_par_vals, **folded_par_dic))
+
+        self.ln_folded_posterior = ln_folded_posterior
+        self.ln_folded_posterior.__signature__ = self.posterior.prior.signature
+
+    @abstractmethod
+    def jump_function(self, samples):
+        """
+        Provided by the subclass.
+        A function that takes a DataFrame of samples and returns another
+        one with the same columns. Needs to have unit Jacobian and be
+        its own inverse.
+        """
+
+    def __call__(self):
+        params = self.posterior.prior.sampled_params
+        jumped_samples = self.jump_function(self.folded_samples)
+
+        np.testing.assert_allclose(self.folded_samples,
+                                   self.jump_function(jumped_samples),
+                                   err_msg='Jump function is not self inverse.')
+
+        # Discard samples for which the jump function is the identity:
+        relevant_rows = ~np.all(self.folded_samples == jumped_samples, axis=1)
+        samples = self.folded_samples[relevant_rows]
+        jumped_samples = jumped_samples[relevant_rows]
+
+        lnprob = self.ln_folded_posterior(**samples[params])
+        lnprob_jumped = self.ln_folded_posterior(**jumped_samples[params])
+
+        jump_probability = np.exp(lnprob_jumped - lnprob)
+        jump = np.random.uniform(0, 1, len(samples)) < jump_probability
+
+        print(f'{100 * np.count_nonzero(jump) / len(self.folded_samples):.1f}%'
+              ' of samples jumped.')
+
+        test_samples = samples.copy()
+        test_samples[jump] = jumped_samples[jump]
+
+        relevant_pars = samples.columns[~np.all(samples == jumped_samples,
+                                                axis=0)]
+        gw_plotting.MultiCornerPlot([samples, test_samples],
+                                    labels=['Original', 'Jump test'],
+                                    params=relevant_pars
+                                   ).plot()
+        return samples, test_samples
 
 
-def psi_jump_function(samples):
+class AutomaticJumpTest(BaseJumpTest):
     """
-    Return a copy of a samples dataframe where the jump has been applied
-    to column 'lnq'.
+    Abstract class for jump tests whose range is set automatically from
+    the samples.
+    Defines attributes ``params`` and ``range_dic``.
+    Provides a method ``jump_function`` and an abstract method
+    ``_aux_jump``.
     """
-    jumped_samples = samples.copy()
-    psi = samples['psi']
-    jumped_samples['psi'] = (
-        psi + np.pi/4 + np.pi/2*((psi%(np.pi/2)) > np.pi/4)
-        ) % np.pi
-    return jumped_samples
+    def __init__(self, posterior, samples, params, quantile=(.01, .99)):
+        """
+        Parameters
+        ----------
+        posterior: cogwheel.posterior.Posterior
 
+        samples: pandas.DataFrame
+            Columns must contain ``posterior.prior.sampled_params``,
+            rows correspond to samples that allegedly come from
+            ``posterior.lnposterior``.
 
-def cumchidiff_jump_function(samples):
-    jumped_samples = samples.copy()
-    jumped_samples['cumchidiff'] = (samples['cumchidiff'] + 1/2) % 1
+        params: list of str
+            Which parameters to involve in the jump function.
+            If you want to jump-test some parameter that has
+            correlations, try reflecting all correlated parameters at
+            once. All parameters must be in
+            ``posterior.prior.sampled_params``.
 
+        quantile: (float, float) between 0 and 1
+            Used to automatically detect the range of values for
+            parameters in ``params`` for which the jump function will be
+            applied, in terms of quantiles of ``samples``.
+        """
+        super().__init__(posterior, samples)
 
-def weighted_quantile(values, quantiles, sample_weight=None,
-                      values_sorted=False, old_style=False):
-    """ Very close to numpy.percentile, but supports weights.
-    NOTE: quantiles should be in [0, 1]!
-    :param values: numpy.array with data
-    :param quantiles: array-like with many quantiles needed
-    :param sample_weight: array-like of the same length as `array`
-    :param values_sorted: bool, if True, then will avoid sorting of
-        initial array
-    :param old_style: if True, will correct output to be consistent
-        with numpy.percentile.
-    :return: numpy.array with computed quantiles.
-    """
-    values = np.array(values)
-    quantiles = np.array(quantiles)
-    if sample_weight is None:
-        sample_weight = np.ones(len(values))
-    sample_weight = np.array(sample_weight)
-    assert np.all(quantiles >= 0) and np.all(quantiles <= 1), \
-        'quantiles should be in [0, 1]'
+        self.params = params
+        self.range_dic = {par: samples[par].quantile(quantile)
+                          for par in params}
 
-    if not values_sorted:
-        sorter = np.argsort(values)
-        values = values[sorter]
-        sample_weight = sample_weight[sorter]
-
-    weighted_quantiles = np.cumsum(sample_weight) - 0.5 * sample_weight
-    if old_style:
-        # To be convenient with numpy.percentile
-        weighted_quantiles -= weighted_quantiles[0]
-        weighted_quantiles /= weighted_quantiles[-1]
-    else:
-        weighted_quantiles /= np.sum(sample_weight)
-    return np.interp(quantiles, weighted_quantiles, values)
-
-def gen_reflection_jump_function(params, samples):
-    """
-    Generate a jump function that is robust to linearly correlated variables.
-    Should work best if ``params`` are unimodal parameters.
-    """
-    def jump_function(samples):
+    def jump_function(self, samples):
         jumped_samples = samples.copy()
-        valid = np.ones(len(samples), dtype=bool)
-        inds = np.arange(len(samples))
-        min_dic = {}
-        max_dic = {}
 
-        # Remove outliers:
-        for par in params:
-            min_dic[par], max_dic[par] = weighted_quantile(
-                samples[valid][par], (0.2, 0.8),
-                samples[valid].get('weights'))
-            valid[inds[valid][samples[par][valid] < min_dic[par]]] = False
-            valid[inds[valid][samples[par][valid] > max_dic[par]]] = False
-
-        for par in params:
-            @np.vectorize
-            def aux_jump(value):
-                if value < min_dic[par] or value > max_dic[par]:
-                    return value
-                return min_dic[par] + max_dic[par] - value
-
-            jumped_samples[par].iloc[inds[valid]] = aux_jump(samples[par][valid])
+        for par, rng in self.range_dic.items():
+            jumped_samples[par] = self._aux_jump(
+                self.folded_samples[par], *rng)
 
         return jumped_samples
-    return jump_function
 
-def gen_shift_jump_function(params, samples):
+    @abstractmethod
+    @staticmethod
+    @np.vectorize
+    def _aux_jump(value, low, high):
+        """
+        Provided by subclass.
+        Return the value at which the parameter will jump given the
+        automatically defined range.
+        """
+
+
+class ReflectionJumpTest(AutomaticJumpTest):
     """
-    Generate a jump function that is robust to linearly correlated variables.
+    Generate a jump function automatically that inverts a range of
+    values in the bulk of the distribution.
     Should work best if ``params`` are unimodal parameters.
     """
-    def jump_function(samples):
-        jumped_samples = samples.copy()
-        valid = np.ones(len(samples), dtype=bool)
-        inds = np.arange(len(samples))
-        min_dic = {}
-        max_dic = {}
+    @staticmethod
+    @np.vectorize
+    def _aux_jump(value, low, high):
+        if low < value < high:
+            return low + high - value
+        return value
 
-        # Remove outliers:
-        for par in params:
-            min_dic[par], max_dic[par] = weighted_quantile(
-                samples[valid][par], (0.2, 0.8),
-                samples[valid].get('weights'))
-            valid[inds[valid][samples[par][valid] < min_dic[par]]] = False
-            valid[inds[valid][samples[par][valid] > max_dic[par]]] = False
 
-        for par in params:
-            @np.vectorize
-            def aux_jump(value):
-                if value < min_dic[par] or value > max_dic[par]:
-                    return value
-                return ((value - min_dic[par] + (max_dic[par]-min_dic[par]) / 2)
-                        % (max_dic[par] - min_dic[par])) + min_dic[par]
-
-            jumped_samples[par].iloc[inds[valid]] = aux_jump(samples[par][valid])
-
-        return jumped_samples
-    return jump_function
+class ShiftJumpTest(AutomaticJumpTest):
+    """
+    Generate a jump function automatically that shifts a range of
+    values in the bulk of the distribution.
+    Should work best if ``params`` are unimodal parameters.
+    """
+    @staticmethod
+    @np.vectorize
+    def _aux_jump(value, low, high):
+        if low < value < high:
+            return (value - low + (high-low)/2) % (high-low) + low
+        return value
