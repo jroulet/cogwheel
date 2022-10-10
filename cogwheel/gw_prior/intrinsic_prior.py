016e17b33b1e4a46b88c72eab4c4408e900f2dc9
Tousif Islam <tousifislam24@gmail.com>
2022-10-10 15:41:02 -0400

    adding prior class for BNS in intrinsic priors

--- cogwheel/gw_prior/intrinsic_prior.py
+++ cogwheel/gw_prior/intrinsic_prior.py
@@ -4,13 +4,22 @@ Priors for estimating only the intrinsic parameter of compact binary mergers
 from cogwheel.prior import CombinedPrior
 from .combined import RegisteredPriorMixin
 from .mass import UniformDetectorFrameMassesPrior
+from .tides import UniformTidalDeformabilitiesBNSPrior
 from .spin import UniformEffectiveSpinPrior, ZeroInplaneSpinsPrior
 from .miscellaneous import ZeroTidalDeformabilityPrior, FixedReferenceFrequencyPrior
 
-
+# for BBH systems
 class IntrinsicParametersPrior(RegisteredPriorMixin, CombinedPrior):
     prior_classes = [UniformDetectorFrameMassesPrior,
                     UniformEffectiveSpinPrior,
                     ZeroInplaneSpinsPrior,
                     ZeroTidalDeformabilityPrior,
                     FixedReferenceFrequencyPrior]
+    
+# for BNS and NSBH systems
+class IntrinsicTidalPrior(RegisteredPriorMixin, prior.CombinedPrior):
+    prior_classes = [UniformTidalDeformabilitiesBNSPrior,
+                     UniformDetectorFrameMassesPrior,
+                     UniformEffectiveSpinPrior,
+                     ZeroInplaneSpinsPrior,
+                     FixedReferenceFrequencyPrior]
