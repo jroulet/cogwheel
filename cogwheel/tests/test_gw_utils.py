358003ca929f618e42e92ac2639243ac9a4796fc
Javier Roulet <jroulet@princeton.edu>
2023-04-13 10:26:18 -0300

    Added test for tensor-product computation of fplus, fcross against LAL.

--- cogwheel/tests/test_gw_utils.py
+++ cogwheel/tests/test_gw_utils.py
@@ -3,35 +3,52 @@
 from unittest import TestCase, main
 import numpy as np
 
-from cogwheel.gw_utils import fplus_fcross, DETECTORS
+import lal
+
+from cogwheel import gw_utils
+from cogwheel import skyloc_angles
 
 
-class PolarizationTestCase(TestCase):
-    """
-    Test sign convention for the sign of the polarization.
-    We define psi such that
-        (F+, Fx) = ((c, s), (-s, c)) @ (F+(psi=0), Fx(psi=0))
-    where c = cos(2 psi), s = sin(2 psi).
-    """
+class AntennaCoefficientsTestCase(TestCase):
+    """Tests for computation of fplus, fcross."""
     @staticmethod
-    def test_psi():
-        """
-        Compute antenna pattern for a random location and polarization
-        and verify that it is the same as if we apply the polarization
-        manually.
-        """
+    def _get_random_args():
         ra = np.random.uniform(0, np.pi)
         dec = np.random.uniform(-np.pi/2, np.pi/2)
-        psi = np.random.uniform(0, np.pi)
         tgps = np.random.uniform(1e9)
+        detector_names = tuple(gw_utils.DETECTORS)
+        return ra, dec, tgps, detector_names
 
-        fp_fc = fplus_fcross(tuple(DETECTORS), ra, dec, psi, tgps)
-        fp0_fc0 = fplus_fcross(tuple(DETECTORS), ra, dec, 0, tgps)
+    def test_psi(self):
+        """
+        Test sign convention for the sign of the polarization.
+        We define psi such that
+            (F+, Fx) = ((c, s), (-s, c)) @ (F+(psi=0), Fx(psi=0))
+        where c = cos(2 psi), s = sin(2 psi).
+        We compute antenna coefficients for a random location and
+        polarization and verify that it is the same as if we apply the
+        polarization manually.
+        """
+        ra, dec, tgps, detector_names = self._get_random_args()
+        psi = np.random.uniform(0, np.pi)
+
+        fp_fc = gw_utils.fplus_fcross(detector_names, ra, dec, psi, tgps)
+        fp0_fc0 = gw_utils.fplus_fcross(detector_names, ra, dec, 0, tgps)
 
         psi_rotation = np.array([[np.cos(2*psi), np.sin(2*psi)],
                                  [-np.sin(2*psi), np.cos(2*psi)]])
 
         np.testing.assert_allclose(fp_fc, psi_rotation @ fp0_fc0)
+
+    def test_fplus_fcross(self):
+        """Test tensor-product computation of (F+, Fx) against LAL."""
+        ra, dec, tgps, detector_names = self._get_random_args()
+        gmst = lal.GreenwichMeanSiderealTime(tgps)
+        lon = skyloc_angles.ra_to_lon(ra, gmst)
+
+        np.testing.assert_allclose(
+            gw_utils.fplus_fcross(detector_names, ra, dec, 0, tgps),
+            gw_utils.get_fplus_fcross_0(detector_names, dec, lon).T)
 
 
 if __name__ == '__main__':
