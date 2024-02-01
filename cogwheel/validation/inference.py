f59a7931f692a20da766f282e702104b39648dd7
Javier Roulet <jroulet@caltech.edu>
2024-02-01 13:43:44 -0500

    Save injection before running PE in case PE gets interrupted.

--- cogwheel/validation/inference.py
+++ cogwheel/validation/inference.py
@@ -222,14 +222,14 @@ def main(config_filename, rundir):
 
     post.likelihood.asd_drift = None  # Set to 1
 
-    sampler = config.SAMPLER_CLS(post, run_kwargs=config.RUN_KWARGS)
-    sampler.run(rundir)
-
     # Save the injection parameters in rundir for extra safety
     event_data.injection['par_dic']['lnl'] = post.likelihood.lnlike_fft(
         event_data.injection['par_dic'])
     with open(rundir/INJECTION_DICT_FILENAME, 'w', encoding='utf-8') as file:
         json.dump(event_data.injection, file, cls=utils.NumpyEncoder, indent=2)
+
+    sampler = config.SAMPLER_CLS(post, run_kwargs=config.RUN_KWARGS)
+    sampler.run(rundir)
 
     make_corner_plot(config, rundir)
 
